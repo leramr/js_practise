c202aaf7017d76ad01fad2afaa1d1b778e304127
leramr <valeriiamorschavka@gmail.com>
2024-07-04 20:37:28 +0300

    pr

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-const textArea = document.querySelector(".text");
-const max = textArea.getAttribute("maxlength");
-const textCounter = document.querySelector(".text-counter");
-textCounter.innerHTML = max;
+const form = document.forms.form
+const textarea = form.text
+console.log (textarea.form)
 
-textArea.addEventListener("keyup", setSymb);
-function setSymb() {
-  const result = max - textArea.value.length;
-  textCounter.innerHTML = result;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// `<form name="some" class="some">
+//      <input type="text" id="example" name="example">
+//    </form>`
+
+
+// const textArea = document.querySelector(".text");
+// const max = textArea.getAttribute("maxlength");
+// const textCounter = document.querySelector(".text-counter");
+// textCounter.innerHTML = max;
+
+// textArea.addEventListener("keyup", setSymb);
+// function setSymb() {
+//   const result = max - textArea.value.length;
+//   textCounter.innerHTML = result;
+// }
