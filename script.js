e47c7add8caf51b8bdb84473128ecc6e7746fd6c
leramr <valeriiamorschavka@gmail.com>
2024-07-05 13:52:57 +0300

    select options

--- script.js
+++ script.js
@@ -1,53 +1,5 @@
 const form = document.forms.form;
+const select = form.select;
 
-const checkbox = form.checkbox;
-
-checkbox[2].value = 'save'
-checkbox[2].checked  = true 
-checkbox[0].value = 'save'
-checkbox[0].checked  = true 
-checkbox[2].value = 'blue'
-console.log (checkbox[2].value)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// `<form name="some" class="some">
-//      <input type="text" id="example" name="example">
-//    </form>`
-
-// const textArea = document.querySelector(".text");
-// const max = textArea.getAttribute("maxlength");
-// const textCounter = document.querySelector(".text-counter");
-// textCounter.innerHTML = max;
-
-// textArea.addEventListener("keyup", setSymb);
-// function setSymb() {
-//   const result = max - textArea.value.length;
-//   textCounter.innerHTML = result;
-// }
+let newOption = new Option ('kkk', 'k', false, false)
+select.prepend(newOption)
