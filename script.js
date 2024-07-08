dce6120328a55f02b8d7d5d72b32bb9dbe6af29a
leramr <valeriiamorschavka@gmail.com>
2024-07-08 18:14:31 +0300

    делегирование событий только дивов

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
-const refs = {
-    container: document.querySelector('.container'),
-    parent: document.querySelector('.parent'),
-    child :document.querySelector('.child'),
-    innerChild : document.querySelector('.inner-child')
+const container = document.querySelector('.container')
+
+container.addEventListener('click', onClick)
+
+function onClick(e){
+    if (e.target.nodeName !== 'BUTTON'){
+        return
+    }
+    console.log (e.target)
 }
 
-refs.container.addEventListener('click', container)
-refs.parent.addEventListener('click', parent)
-refs.child.addEventListener('click', child)
-refs.innerChild.addEventListener('click', innerChild)
-
-function container (e){
-    console.log ('container')
-    console.log ('container -> target', e.target)
-    console.log ('container ->', e.currentTarget)
-}
-function parent (e){
-    console.log ('parent')
-    console.log ('parent -> target', e.target)
-    console.log ('parent ->', e.currentTarget)
-}
-function child (e){
-    console.log ('child')
-    console.log ('child -> target', e.target)
-    console.log ('child ->', e.currentTarget)
-}
-function innerChild (e){
-    console.log ('innerChild')
-    console.log ('innerChild -> target', e.target)
-    console.log ('innerChild ->', e.currentTarget)
-}
