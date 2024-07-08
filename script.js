b6d07cd0dfd7e08b4b7de60971435adae8bda1f5
leramr <valeriiamorschavka@gmail.com>
2024-07-08 16:36:10 +0300

    fetch, response.json

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const form = document.forms.form
-const input = form.text
-
-input.addEventListener('blur', function (event){
-    form.submit()
-})
 
 
+fetch('https://pokeapi.co/api/v2/pokemon/2')
+  .then(response => {
+    return response.json(); 
+  })
+  .then(response => {
+    console.log(response); 
+  })
+  .catch(error => {
+    console.log(error); 
+  });
