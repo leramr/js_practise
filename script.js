9eedf1ca3d89e6fefa619a38484c270e65fe209d
leramr <valeriiamorschavka@gmail.com>
2024-07-08 17:48:03 +0300

    делегирование событий

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+const refs = {
+    container: document.querySelector('.container'),
+    parent: document.querySelector('.parent'),
+    child :document.querySelector('.child'),
+    innerChild : document.querySelector('.inner-child')
+}
 
+refs.container.addEventListener('click', container)
+refs.parent.addEventListener('click', parent)
+refs.child.addEventListener('click', child)
+refs.innerChild.addEventListener('click', innerChild)
 
-fetch('https://pokeapi.co/api/v2/pokemon/2')
-  .then(response => {
-    return response.json(); 
-  })
-  .then(response => {
-    console.log(response); 
-  })
-  .catch(error => {
-    console.log(error); 
-  });
+function container (e){
+    console.log ('container')
+    console.log ('container -> target', e.target)
+    console.log ('container ->', e.currentTarget)
+}
+function parent (e){
+    console.log ('parent')
+    console.log ('parent -> target', e.target)
+    console.log ('parent ->', e.currentTarget)
+}
+function child (e){
+    console.log ('child')
+    console.log ('child -> target', e.target)
+    console.log ('child ->', e.currentTarget)
+}
+function innerChild (e){
+    console.log ('innerChild')
+    console.log ('innerChild -> target', e.target)
+    console.log ('innerChild ->', e.currentTarget)
+}
