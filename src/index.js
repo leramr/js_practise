8cf8fc219def09b4d23ba13ff5fb0613eca9b3c2
leramr <valeriiamorschavka@gmail.com>
2024-07-16 16:32:48 +0300

    localStorage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
+import { throttle } from "lodash.throttle";
+
+
+
 const refs ={
-    form : document.forms.form
+    form : document.querySelector('.feedback-form'),
+    textarea : document.querySelector('.textarea')
 }
-console.log (refs.form)
+
+const STORAGE_MESSAGE = 'feedback-message'
+populatedTextarea()
+
+refs.form.addEventListener('submit', onFormSubmit)
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 1000))
+
+function onFormSubmit(e){
+e.preventDefault ()
+console.log ('send')
+e.target.reset()
+localStorage.removeItem(STORAGE_MESSAGE)
+}
+
+function onTextareaInput(e){
+    const message = e.target.value
+    localStorage.setItem(STORAGE_MESSAGE, message)
+}
+
+function populatedTextarea(){
+const savedMessage = localStorage.getItem(STORAGE_MESSAGE)
+if (savedMessage){
+    console.log (savedMessage)
+    refs.textarea.value=savedMessage
+}
+}
+
